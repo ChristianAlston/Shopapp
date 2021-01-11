9e79b20cea0f47bcaab5f43c07ed34f5972e0e58
Christian Alston <xcda15@gmail.com>
2021-01-10 21:54:38 -0500

    dom manipulation for full screen view of pictures added. need to finish toggle function

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// nav 
+
 const nav = document.querySelector( '.navscreen' )
 
 const burger = document.querySelector( '.burger' )
@@ -20,8 +22,6 @@ const openNav = () => {
     nav.classList.toggle( 'open' )
 }
 
-
-
 burger.addEventListener( "click", () => {
     openNav();
     game.classList.toggle( 'a' )
@@ -33,4 +33,52 @@ burger.addEventListener( "click", () => {
     links.forEach( ( link ) => {
         link.classList.toggle( 'appear' )
     } )
+} )
+
+
+
+// fullscreen pictures
+
+const nike1 = document.querySelector( '.productimage1' )
+
+const nike2 = document.querySelector( '.productimage2' )
+
+const nike3 = document.querySelector( '.productimage3' )
+
+
+const full = document.querySelector( '.full' )
+
+const full2 = document.querySelector( '.full2' )
+
+const full3 = document.querySelector( '.full3' )
+
+const fullscreen = () => {
+    full.classList.toggle( 'fullscreen' )
+
+}
+
+const fullscreen2 = () => {
+    full2.classList.toggle( 'fullscreen' )
+}
+
+const fullscreen3 = () => {
+    full3.classList.toggle( 'fullscreen' )
+}
+
+nike1.addEventListener( "click", () => {
+    fullscreen()
+} )
+
+nike2.addEventListener( "click", () => {
+    fullscreen2()
+} )
+
+nike3.addEventListener( "click", () => {
+    fullscreen3()
+} )
+
+document.addEventListener( "click", ( e ) => {
+    if ( e.target == full ) {
+        full.classList.remove( 'fullscreen' )
+    }
 } )
